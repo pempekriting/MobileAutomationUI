3070b20ef8ccd3796cee26a97571a597aa6e8df9
Azzam Nizar <kgs.azzam@gmail.com>
2022-04-11 14:07:59 +0700

    Fixing error

--- src/main/java/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class Constants {
-    public static final String PROJECT_PATH = System.getProperty("user.dir");
-    public static final String APK = PROJECT_PATH + "/src/test/resources/apk/EriBank.apk";
-    public static final String APK_SERVER = "/root/tmp/apk/EriBank.apk";
-}
--- src/test/resources/features/Login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Login to Eri Bank
-
-  As an user, I can login to the Eri Bank Apps
-
-  Scenario: Login with valid username and password
-    When User provide valid username and password
-    Then User directed to the products page
